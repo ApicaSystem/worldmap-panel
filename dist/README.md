420848c1336139d53ac5dd82b412618461b29c87
abalobanov <alexander.balobanov@apicasystem.com>
2017-11-09 23:00:54 +0100

    WPMX-3892: Apply default panel properties. Adjust Editor form so it doesn't show useless settings.

--- dist/README.md
+++ dist/README.md
@@ -10,4 +10,17 @@ The original readme file can be found here: [README.md](https://github.com/grafa
 ## v0.0.1
 
 Based on [Grafana Worldmap Panel plugin](https://github.com/grafana/worldmap-panel) v0.0.18.
+The only format from datasource is supported: table.
+Required the following 'columns' in table:
+- geohash - encoded latitude and longitude
+- metric - takes a color code: 0 for gray, 1 for green, 2 for yellow, 3 for orange and 4 for red
+- a column with popup label text that will be shown on mouse over the point on the map, optional, if nothing then n/a will be shown as a popup label.
 
+Editor tab is simplified. The following settings can be found here:
+Map Visual Options:
+- Center - for centering the map
+- Initial Zoom
+- Min Circle Size - no different sizes are allowed, so this field sets all circles size
+- Sticky Labels - if checked then popup labels will not disappear automatically
+Map Data Options:
+- Table Label Field - table column that should be used as popup label text. Predefined with 'summaryHtml' value, which is the column name that contains Apica Syntetic Monitoring location checks summary. 
--- dist/worldmap_ctrl_apica.js
+++ dist/worldmap_ctrl_apica.js
@@ -1,9 +1,9 @@
 'use strict';
 
-System.register(['./worldmap_ctrl'], function (_export, _context) {
+System.register(['./worldmap_ctrl', 'jquery'], function (_export, _context) {
   "use strict";
 
-  var WorldmapCtrlOriginal, WorldmapCtrl;
+  var WorldmapCtrlOriginal, $, _createClass, _get, panelDefaultsApica, WorldmapCtrl;
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -38,16 +38,117 @@ System.register(['./worldmap_ctrl'], function (_export, _context) {
   return {
     setters: [function (_worldmap_ctrl) {
       WorldmapCtrlOriginal = _worldmap_ctrl.default;
+    }, function (_jquery) {
+      $ = _jquery.default;
     }],
     execute: function () {
+      _createClass = function () {
+        function defineProperties(target, props) {
+          for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+          }
+        }
+
+        return function (Constructor, protoProps, staticProps) {
+          if (protoProps) defineProperties(Constructor.prototype, protoProps);
+          if (staticProps) defineProperties(Constructor, staticProps);
+          return Constructor;
+        };
+      }();
+
+      _get = function get(object, property, receiver) {
+        if (object === null) object = Function.prototype;
+        var desc = Object.getOwnPropertyDescriptor(object, property);
+
+        if (desc === undefined) {
+          var parent = Object.getPrototypeOf(object);
+
+          if (parent === null) {
+            return undefined;
+          } else {
+            return get(parent, property, receiver);
+          }
+        } else if ("value" in desc) {
+          return desc.value;
+        } else {
+          var getter = desc.get;
+
+          if (getter === undefined) {
+            return undefined;
+          }
+
+          return getter.call(receiver);
+        }
+      };
+
+      panelDefaultsApica = {
+        locationData: 'table',
+        tableLabel: 'summaryHtml',
+        circleMinSize: 5,
+        circleMaxSize: 5,
+        showLegend: false,
+
+        thresholds: '1,2,3,4',
+        colors: ['rgba(90, 98, 90, 0.9)', 'rgba(50, 172, 45, 0.97)', 'rgba(224, 210, 0, 0.97)', 'rgba(237, 129, 40, 0.89)', 'rgba(245, 54, 54, 0.9)']
+      };
+
       WorldmapCtrl = function (_WorldmapCtrlOriginal) {
         _inherits(WorldmapCtrl, _WorldmapCtrlOriginal);
 
         function WorldmapCtrl($scope, $injector, contextSrv) {
           _classCallCheck(this, WorldmapCtrl);
 
-          return _possibleConstructorReturn(this, (WorldmapCtrl.__proto__ || Object.getPrototypeOf(WorldmapCtrl)).call(this, $scope, $injector, contextSrv));
+          var _this = _possibleConstructorReturn(this, (WorldmapCtrl.__proto__ || Object.getPrototypeOf(WorldmapCtrl)).call(this, $scope, $injector, contextSrv));
+
+          // tableLabel is not initiated in parent by default, so this is the mark that here we should init Apica checks summary by location format. 
+          if (!_this.panel.tableLabel) {
+            _.assign(_this.panel, _.cloneDeep(panelDefaultsApica));
+          }
+
+          _this.events.on('render', _this.tweakOriginalEditor.bind(_this));
+          _this.events.on('refresh', _this.tweakOriginalEditor.bind(_this));
+          return _this;
         }
+
+        _createClass(WorldmapCtrl, [{
+          key: 'changeTab',
+          value: function changeTab(newIndex) {
+            var _this2 = this;
+
+            _get(WorldmapCtrl.prototype.__proto__ || Object.getPrototypeOf(WorldmapCtrl.prototype), 'changeTab', this).call(this, newIndex);
+            setTimeout(function () {
+              _this2.tweakOriginalEditor();
+            }, 100);
+          }
+        }, {
+          key: 'tweakOriginalEditor',
+          value: function tweakOriginalEditor() {
+            // hide not useless Map Visual Options:
+            // - Max Circle Size - circle sizes should be the same for now
+            // - Decimals - metric is used for the color, so decimals settings is useless
+            // - Unit - same as Decimals
+            // - Show Legend - legend shows metric values, but they as color codes are useless
+            // - Location Data - should always be 'table' (ASM Datasource provides data in table format)
+            // - Aggregation - not used, 'metric' property from data source is always used
+            $('.tabbed-view-body').find('\n        input[ng-model="ctrl.panel.circleMaxSize"],\n        input[ng-model="ctrl.panel.decimals"],\n        input[ng-model="ctrl.panel.unitSingular"],\n        gf-form-switch[checked="ctrl.panel.showLegend"],\n        select[ng-model="ctrl.panel.locationData"],\n        select[ng-model="ctrl.panel.valueName"]\n      ').closest('.gf-form').hide();
+
+            // hide not useless Map Visual Options:
+            // - Threshold Options (Thresholds, Colors) - thresholds are used for circles coloring that 
+            // - Hide series (With only nulls, With only zeros)
+            $('.tabbed-view-body').find('\n        input[ng-model="ctrl.panel.thresholds"],\n        gf-form-switch[checked="ctrl.panel.hideEmpty"]\n      ').closest('.gf-form-group').hide();
+          }
+        }, {
+          key: 'render',
+          value: function render() {
+            // different circle sizes in current implementation has no sence, so we adjust them to be equal.
+            this.panel.circleMaxSize = this.panel.circleMinSize;
+            _get(WorldmapCtrl.prototype.__proto__ || Object.getPrototypeOf(WorldmapCtrl.prototype), 'render', this).call(this);
+          }
+        }]);
 
         return WorldmapCtrl;
       }(WorldmapCtrlOriginal);
